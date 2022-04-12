84d2acb17384501978822b24885d05c19f028aa1
Maximiliano Lombardia <mlombardia@itba.edu.ar>
2022-04-12 09:57:42 -0300

    feat: early modelling and particle generation (#1)
    * feat: early modelling and particle generation
    
    * Update ParticleGenerator.java
    
    * Update ParticleGenerator.java
    
    * Update ParticleGenerator.java

---
+++ src/main/java/model/Particle.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Particle extends Element {
+
+  public double xVel;
+  public double yVel;
+
+  public Particle(double xPos, double yPos, double weight, double radius, double xVel, double yVel) {
+    super(xPos, yPos, weight, radius);
+    this.xVel = xVel;
+    this.yVel = yVel;
+  }
+
+  public void calculateXPosition(double time) {
+    this.xVel += (this.xVel * time);
+  }
+
+  public void calculateYPosition(double time) {
+    this.yVel += (this.yVel * time);
+  }
+}
---
+++ src/main/java/model/Wall.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Wall extends Element {
+  public Wall(double xPos, double yPos) {
+    super(xPos, yPos, 0, 0);
+  }
+}
