fd9b3e1c85144087fc70231dd11fabf46d0fb2d1
Maximiliano Lombardia <mlombardia@itba.edu.ar>
2022-04-20 00:16:12 -0300

    impact time v1 finished

--- src/main/java/engine/SimulationController.java
+++ src/main/java/engine/SimulationController.java
@@ -18,11 +18,10 @@ public class SimulationController {
     TreeMap<Double, Element[]> events = new TreeMap<>();
 
     for (Particle p : SystemGenerator.particles) {
-      Wall w = new Wall();
+      Wall w;
       if (p.xVel > 0) {
         if (p.xPos > SystemGenerator.width/2) {
           w = SystemGenerator.getWall(3);
-          // wallPos = w.yPos + p.yPos;
         } else {
           if (p.yPos > (((SystemGenerator.height - D) / 2) + D)){
             w = SystemGenerator.getWall(5);
@@ -30,7 +29,7 @@ public class SimulationController {
             w = SystemGenerator.getWall(4);
           }
         }
-        // events.put(calculateImpactTime(p.xVel,p.radius,p.xPos,w.yPos),new Element[]{p,w});
+        events.put(calculateImpactTime(w.xPos, -p.radius, p.xPos, p.xVel),new Element[]{p,w});
       } else {
         if (p.xPos > SystemGenerator.width / 2) {
           if (p.yPos > (((SystemGenerator.height - D) / 2) + D)) {
@@ -41,21 +40,30 @@ public class SimulationController {
         } else {
           w = SystemGenerator.getWall(2);
         }
-        events.put(calculateImpactTime(p,w),new Element[]{p,w});
+        events.put(calculateImpactTime(w.xPos, p.radius, p.xPos, p.xVel),new Element[]{p,w});
       }
 
       if (p.yVel > 0) {
         w = SystemGenerator.getWall(0);
-        events.put(calculateImpactTime(p,w),new Element[]{p,w});
+        events.put(calculateImpactTime(w.yPos, -p.radius, p.yPos, p.yVel),new Element[]{p,w});
       } else {
         w = SystemGenerator.getWall(1);
-        events.put(calculateImpactTime(p,w),new Element[]{p,w});
+        events.put(calculateImpactTime(w.yPos, p.radius, p.yPos, p.yVel),new Element[]{p,w});
       }
     }
 
-    for (Particle p1 : SystemGenerator.particles) {
-      for (Particle p2 : SystemGenerator.particles) {
+    for (Particle pi : SystemGenerator.particles) {
+      for (Particle pj : SystemGenerator.particles) {
+        double[] deltaV = new double[]{pj.xVel - pi.xVel, pj.yVel - pi.yVel};
+        double[] deltaR = new double[]{pj.xPos - pi.xPos, pj.yPos - pi.yPos};
+        double sigma = pj.radius + pi.radius;
+        double VRProduct = scalarVectorMultiply(deltaV, deltaR);
+        double VVProduct = scalarVectorMultiply(deltaV, deltaV);
+        double d = Math.pow(VRProduct,2) - (VVProduct * (scalarVectorMultiply(deltaR, deltaR) - Math.pow(sigma, 2)));
 
+        if(!isInfinite(VRProduct,d)){
+          events.put(calculateImpactTime(VRProduct, d, VVProduct), new Element[]{pi,pj});
+        }
       }
     }
     return events.ceilingEntry(events.firstKey()); //CHECK
@@ -68,8 +76,20 @@ public class SimulationController {
     }
   }
 
-  private static double calculateImpactTime(Particle p, Wall w){
-    return 0;
+  private static double calculateImpactTime(double wallPos, double radius, double particlePos, double particleVel){
+    return (wallPos + radius + particlePos)/particleVel;
+  }
+
+  private static double calculateImpactTime(double VR, double d, double VV){
+    return -(VR + Math.sqrt(d))/VV;
+  }
+
+  private static double scalarVectorMultiply(double[] v1, double[] v2) {
+    return (v1[0] * v2[0]) + (v1[1] * v2[1]);
+  }
+
+  private static boolean isInfinite(double VRProduct, double d){
+    return VRProduct >= 0 || d < 0;
   }
 
   public static void saveState() {
